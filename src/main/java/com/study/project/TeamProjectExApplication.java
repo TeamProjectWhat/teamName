8d4ae6e324adaee14db3ac258b4e9a63bc323fa9
GREEN <GREEN@G704-05>
2024-08-23 10:46:18 +0900

    project created

---
+++ src/main/java/com/study/project/TeamProjectExApplication.java
@@ -0,0 +1,13 @@
+package com.study.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjectExApplication.class, args);
+	}
+
+}
